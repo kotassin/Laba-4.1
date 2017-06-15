29376a589d1f77f74f1bffbaeaa535a64d99d608
Kotassin <nastya_navros@mail.ru>
2017-06-15 20:13:01 +0300

    4 commit

--- Laba 4.1/Main.cpp
+++ Laba 4.1/Main.cpp
@@ -22,7 +22,10 @@ int main() {
 	
 	c.show();
 	
-	mot[1].toSetWorth(1000); mot[2].toSetWorth(1500); mot[3].toSetWorth(2000);
+	mot[1].toSetWorth(1000);
+	mot[2].toSetWorth(1500);
+	mot[3].toSetWorth(2000);
+	
 	mot[1].toSetBrand();
 	mot[1].toSetWheels(20);
 	mot[1].toSetPass(5);
@@ -30,12 +33,12 @@ int main() {
 	
 	mot[1] = mot[1] + mot[2];
 	cout << " Total cost: " << mot[1].toGetWorth() << endl;
-	//cout << mot[1].toGetWorth() << endl << mot[2].toGetWorth() << endl << mot[3].toGetWorth() << endl;
+
 
 	mot[1] = mot[1] - mot[2]; cout << " Difference: " << mot[1].toGetWorth() << endl;
 	mot[1] = mot[1] * mot[2]; cout << " Multiplication: " << mot[1].toGetWorth() << endl;
 	mot[1] = mot[2]; cout << " Assignment: " << mot[1].toGetWorth() << endl;
-	if (mot[2] == mot[3]) cout << " The cost of 2 and 3 motorcycles is the same." << endl;
+	if (mot[1] == mot[2]) cout << " The cost of 2 and 3 motorcycles is the same." << endl;
 	else cout << " The cost of 2 and 3 motorcycles isn't the same" << endl;
 
 	system("pause");
